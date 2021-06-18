78ddebe5057789a039144348595f038f2bf8ef03
luffy981 <jhonsmith981@hotmail.com>
2021-06-18 04:14:02 +0000

    Function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+* main - Function main
+* Return: 0
+*/
+int main(void)
+{
+    /*Function puts*/
+char Holberton[] = "\"Programming is like building a multilingual puzzle";
+puts(Holberton);
+return (0);
+}
