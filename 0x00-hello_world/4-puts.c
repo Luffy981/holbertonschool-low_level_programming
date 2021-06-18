026a2b85e43ed9ffecf4cf886cb5a1713d006254
luffy981 <jhonsmith981@hotmail.com>
2021-06-18 04:23:30 +0000

    Function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 /**
 * main - Function main
+* Using puts to print a string
 * Return: 0
 */
 int main(void)
 {
-    /*Function puts*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
