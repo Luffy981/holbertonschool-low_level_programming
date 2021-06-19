e72ff8e228ba65400875b2761e423951a209e82f
luffy981 <jhonsmith981@hotmail.com>
2021-06-19 00:51:55 +0000

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Function main
+ * Return: Return value "n"
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = "A" ; ch <= "Z" ; ch++)
+		putchar(ch\n);
+	for (ch = "a" ; ch <= "z" ; ch++)
+		putchar(ch\n);
+	return (0);
+}
