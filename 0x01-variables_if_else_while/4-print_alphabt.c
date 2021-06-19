e98687aceb43e7a0484a41165e9d892f0a61a9c6
luffy981 <jhonsmith981@hotmail.com>
2021-06-19 15:20:12 +0000

    The alphabet without q , e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Function main
+ * Return: Return value "n"
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i != 'e', 'q' ; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
