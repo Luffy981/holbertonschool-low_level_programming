29fa72b725ad0cb5a87f5620bae47be9a996ac9b
luffy981 <jhonsmith981@hotmail.com>
2021-06-19 03:29:34 +0000

    Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,8 @@ int main(void)
 {
 	char i;
 
-	for (i = 'A'; i <= 'Z'; i++)
-		putchar("%c ", i);
-
-
 	for (i = 'a'; i <= 'z'; i++)
-		putchar("%c ", i);
-
+		putchar(i);
+	putchar(\n);
 	return (0);
 }
