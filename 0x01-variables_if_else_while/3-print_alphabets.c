36b26a1455b9608caecbe6447345e993d8de4f05
luffy981 <jhonsmith981@hotmail.com>
2021-06-19 04:50:14 +0000

    Alphabet upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Function main
+ * Return: Return value "n"
+ */
+int main(void)
+{
+	for (i == 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	for (i == 'A' ; i <= 'Z' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\0');
+	return (0);
+}
