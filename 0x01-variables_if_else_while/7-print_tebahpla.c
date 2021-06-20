47a831fcfc8b0deab68e70096a4ce062ad876d2e
luffy981 <jhonsmith981@hotmail.com>
2021-06-20 19:31:54 +0000

    Reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Function main
+ * Return: Return value "n"
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i <= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
