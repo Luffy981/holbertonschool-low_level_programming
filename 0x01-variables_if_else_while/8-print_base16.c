94d15ff5c07876190adc6234d296ba33170a4f27
luffy981 <jhonsmith981@hotmail.com>
2021-06-20 21:30:03 +0000

    Numbers in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Function main
+ * Return: Return value "n"
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= f ; i++)
+	{
+		putchar(i);
+	}
+	putchar (\n);
+	return (0);
+}
