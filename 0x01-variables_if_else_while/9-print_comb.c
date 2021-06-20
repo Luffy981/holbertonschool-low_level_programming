64603945d1c4c2a8061632df645b3f99c67db2b3
luffy981 <jhonsmith981@hotmail.com>
2021-06-20 22:02:02 +0000

    Combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Function main
+ * Return: Return value "n"
+ */
+int main(void)
+{
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar("i \n");
+	}
+	putchar ('\n');
+	return (0);
+}
