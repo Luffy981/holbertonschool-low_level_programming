e31ed4d53bd6f71f6c4921f7b163ee1deebebfa7
luffy981 <jhonsmith981@hotmail.com>
2021-06-22 23:10:00 +0000

    Phrase using _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "_putchar.c"
+/**
+ * main - Function main
+ * Return: Return value "n"
+ */
+int main(void)
+{
+	char (H);
+	char (o);
+	char (l);
+	char (b);
+	char (e);
+	char (r);
+	char (t);
+	char (n);
+
+	_putchar("H");
+	_putchar("o");
+	_putchar("l");
+	_putchar("b");
+	_putchar("e");
+	_putchar("r");
+	_putchar("t");
+	_putchar("o");
+	_putchar("n");
+	_putchar('\n');
+	return (0);
+}
