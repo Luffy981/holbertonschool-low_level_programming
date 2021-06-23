ba5087172392e52c3f9606e7dea02c4c7f39ef70
luffy981 <jhonsmith981@hotmail.com>
2021-06-23 04:57:26 +0000

    Function _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _islower - Function to return 1 if C is a lower and 0 if C is upper
+ *@c: Parameter
+ * Return: Return value "n"
+ */
+int _islower(int c)
+{
+	if ((c >= 65 && c <= 90))
+	{
+		return (0);
+	}
+	if ((c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
