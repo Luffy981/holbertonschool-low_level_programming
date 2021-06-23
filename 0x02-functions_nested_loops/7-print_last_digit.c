25e7f50a6a841784239e9fb69330e7a6abb45a2d
luffy981 <jhonsmith981@hotmail.com>
2021-06-23 22:23:44 +0000

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * print_last_digit - Function to return 1 if C is a lower and 0 if C is upper
+ *@u: Parameter
+ * Return: Return value "n"
+ */
+int print_last_digit(int u)
+{
+	u = u % 10;
+	_putchar(u);
+	return (0);
+}
