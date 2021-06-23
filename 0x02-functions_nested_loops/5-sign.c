30d6951e10d5d1127594cc3a79aba9e7e60cf19b
luffy981 <jhonsmith981@hotmail.com>
2021-06-23 06:10:28 +0000

    Function print_sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,20 @@
 #include "holberton.h"
+/**
+ * print_sign - Function to return 1 if C is a lower and 0 if C is upper
+ *@n: Parameter
+ * Return: Return value "n"
+ */
 int print_sign(int n)
 {
-	if(n >= '0')
+	if (n >= '0')
 	{
 		_putchar(43);
 		return (1);
-	} else if(n <= '0')
+	} else if (n <= '0')
 	{
 		_putchar(45);
 		return (-1);
-	} else if((n = '0'))
+	} else if ((n == '0'))
 	{
 		_putchar(48);
 		return (0);
