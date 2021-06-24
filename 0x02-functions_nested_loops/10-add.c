7fc175ee3c6f87ab6f0fc6628a383c05c6022320
luffy981 <jhonsmith981@hotmail.com>
2021-06-24 05:44:32 +0000

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - Function to return 1 if C is a lower and 0 if C is upper
+ *@a: Parameter
+ *@b: Parameter
+ *@r: Parameter
+ * Return: Return value "n"
+ */
+int add(int a, int b)
+{
+	int r;
+
+	r =(a + b);
+	return(r);
+}
