69242734c0095b6ecf740a6a68ab1eed376f9207
luffy981 <jhonsmith981@hotmail.com>
2021-06-24 05:12:50 +0000

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
  /**
- * print_alphabet - Function to print lower alphabet
- * Return: Return value "n"
+ * times_table - Function to print lower alphabet
  */
 void times_table(void)
 {
-	int a=0;
-	int b=0;
+	int a = 0;
+	int b = 0;
 	int r;
+
 	for (a = 0 ; a <= 9 ; a++)
 	{
 		for (b = 0 ; b <= 9 ; b++)
 		{
-			r = a *(b);
+			r = a * (b);
 			if (b != 0)
 			{
 				_putchar(',');
@@ -20,8 +20,8 @@ void times_table(void)
 			}
 			if (r >= 10)
 			{
-				_putchar((r/10) + '0');
-				_putchar((r%10) + '0');
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
 			} else if (r < 10)
 			{
 				_putchar(' ');
