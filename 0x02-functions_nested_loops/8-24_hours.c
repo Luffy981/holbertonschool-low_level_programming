10aaf8235b9d4a133e5e55e7941fe53e617ed29a
luffy981 <jhonsmith981@hotmail.com>
2021-06-24 01:39:52 +0000

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,20 +1,18 @@
 #include "holberton.h"
 /**
- * print_last_digit - Function to return 1 if C is a lower and 0 if C is upper
- *@u: Parameter
- *
+ * jack_bauer - Function to return 1 if C is a lower and 0 if C is upper
  * Return: Return value "n"
  */
 void jack_bauer(void)
 {
-	int a=0;
-	int b=0;
-	int c=0;
-	int d=0;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
 
 	for (a = 0 ; a <= 2 ; a++)
 	{
-		for (b = 0 ; b <= 3 ; b ++)
+		for (b = 0 ; b <= 3 ; b++)
 		{
 			for (c = 0 ; c <= 5 ; c++)
 			{
