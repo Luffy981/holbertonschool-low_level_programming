f7a90f78b9db099e666dce8dce9a1bd8a68ea184
luffy981 <jhonsmith981@hotmail.com>
2021-06-24 21:31:57 +0000

    Returns 1 if c is a digit Returns 0 otherwise

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Function to print lower alphabet
+ * Return: Return value "n"
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c <=57)
+	{
+		return (1);
+	}
+	return (0);
+}
