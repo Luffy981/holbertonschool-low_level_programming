c796161396ad90e864c1f5ecfc62d8e4059ea347
luffy981 <jhonsmith981@hotmail.com>
2021-06-24 00:41:12 +0000

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,9 +2,16 @@
 /**
  * print_last_digit - Function to return 1 if C is a lower and 0 if C is upper
  *@u: Parameter
+ *
  * Return: Return value "n"
  */
 int print_last_digit(int u)
 {
-	return (u %= u);
+	u %= 10;
+	if (u < 0)
+	{
+		u = (u * (-1));
+	}
+	_putchar('0' + u);
+	return (u);
 }
