8e4b5eb985579e214db2dcf930d3590d12e2387c
luffy981 <jhonsmith981@hotmail.com>
2021-06-24 02:01:53 +0000

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,25 +5,31 @@
  */
 void jack_bauer(void)
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int d = 0;
+	int a;
+	int b;
+	int c;
+	int d;
 
 	for (a = 0 ; a <= 2 ; a++)
 	{
-		for (b = 0 ; b <= 3 ; b++)
+		for (b = 0 ; b <= 9 ; b++)
 		{
 			for (c = 0 ; c <= 5 ; c++)
 			{
 				for (d = 0 ; d <= 9 ; d++)
 				{
+					if (a == 2 && b > 3)
+					{
+						break;
+					} else
+					{
 					_putchar('0' + a);
 					_putchar('0' + b);
 					_putchar(':');
 					_putchar('0' + c);
 					_putchar('0' + d);
 					_putchar('\n');
+					}
 				}
 			}
 		}
