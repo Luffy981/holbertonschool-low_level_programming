bd2a31e83c031550c52b7bb5f799366376c2434d
luffy981 <jhonsmith981@hotmail.com>
2021-06-25 12:32:39 +0000

    prints a triangle with #

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * print_diagonal - Function to print \ in diagonal
- * @n: Parameter
+ * print_triangle - Function to print triangle
+ * @size: Parameter
  * Return: Return value "n"
  */
 void print_triangle(int size)
@@ -10,23 +10,24 @@ void print_triangle(int size)
 	int i;
 	int j;
 	int k;
-	n=size;
-	if(n>=1)
+
+	n = size;
+	if (n >= 1)
 	{
-		for(i=n ; i >0 ; i--)
+		for (i = n ; i > 0 ; i--)
 		{
-			for(j=0; j<(i-1); j++)
+			for (j = 0 ; j < (i - 1) ; j++)
 			{
 				_putchar(' ');
 			}
-			for(k=0 ; k <=n-i  ; k++)
+			for (k = 0 ; k <= (n - i) ; k++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
 		}
 	}
-	if(n<1)
+	if (n < 1)
 	{
 		_putchar('\n');
 	}
