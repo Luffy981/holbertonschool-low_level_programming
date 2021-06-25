f0897642d3c116393ab57d4b3c73bea1a3bb94dd
luffy981 <jhonsmith981@hotmail.com>
2021-06-25 00:41:22 +0000

    function  draws a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 /**
- * times_table - Function to print table
+ * print_line - Function to print table
+ * @n: Parameter
  * Return: Return value "n"
  */
 void print_line(int n)
 {
 	int i;
-	if (n>=1)
+
+	if (n >= 1)
 	{
-		for(i=0;i<n;i++)
+		for (i = 0 ; i < n ; i++)
 		{
 			_putchar('_');
 		}
