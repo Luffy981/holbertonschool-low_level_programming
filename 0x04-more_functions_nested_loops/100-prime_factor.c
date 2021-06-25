206b575dc70cc0749cba87a0deaf479d5bba0c59
luffy981 <jhonsmith981@hotmail.com>
2021-06-25 16:42:01 +0000

    Prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,13 +8,13 @@ int main(void)
 	long int a = 612852475143;
 	int i;
 
-	for (i = 2 ; a > 1 ; i++)
+	for (i = 2 ; i < a ; i++)
 	{
 		while (a % i == 0)
 		{
 			a = a / i;
 		}
 	}
-	printf("%d\n", i);
+	printf("%d\n",i);
 	return (0);
 }
