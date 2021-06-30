ee59b93c1ba94078c1f8e24c22e2b02e29f83792
luffy981 <jhonsmith981@hotmail.com>
2021-06-30 01:47:18 +0000

    Function to print an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_array - Function to print a complete array
+ * @a: Parameter
+ * @n: Parameter
+ */
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0 ; b < n ; b++)
+	{
+		printf("%d", a[b]);
+		if (b == n - 1)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+}
