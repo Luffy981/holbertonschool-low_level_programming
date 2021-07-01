45ac4392c1d0f4be961a4293ed8ba7bec062070b
luffy981 <jhonsmith981@hotmail.com>
2021-07-01 23:09:25 +0000

    function  copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- *_strncat - Function to concatenates two strings
+ *_strncpy - Function to concatenates two strings
  * @dest: Parameter
  * @src: Parameter
  * @n: Parameter
@@ -9,11 +9,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	i = 0;
 	while (dest[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
- 		i++;
+		i++;
 	}
 	return (dest);
 }
