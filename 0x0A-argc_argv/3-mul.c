5186a0c9fb5a2fd170ad5d5e39218a881279f8f1
luffy981 <jhonsmith981@hotmail.com>
2021-07-09 18:39:19 +0000

    program that multiplies  numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Function to print the file name
+ * @argc: Parameter The number of command line arguments
+ * @argv: Parameter An array containing the program command line arguments
+ * Return: Return value
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1 ; i < argc ; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		return (printf("%d\n", mul));
+	}
+	printf("Error\n");
+	return (1);
+}
