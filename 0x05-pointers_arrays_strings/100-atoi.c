866e425a5aa578e4aa61add48cd8e5fa4e391aa3
luffy981 <jhonsmith981@hotmail.com>
2021-07-10 05:16:44 +0000

    function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,27 @@
 #include "holberton.h"
-/**
- * _atoi - Function to print a integer with sign
- * @s: Parameter
- * Return: return value
- */
-int _atoi(char *s)
-{
-	int i;
-	int a;
-	int sign;
-	unsigned int d;
 
-	sign = 1;
-	i = 0;
-	while (!(s[i] >= '0' && s[i] <= '9'))
-	{
-		if (s[i] == 45)
-		{
-			sign *= (-1);
-		}
-		i++;
-	}
-	d = 0;
-	a = 0;
-	while (s[a] != '\0')
-	{
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			d = (s[a] - '0') + d * 10;
-		}
-		a++;
-	}
-	return (d * sign);
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value of the converted string.
+ */
+int _atoi(char s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (s == '-')
+			sign = -1;
+
+		else if (s >= '0' && *s <= '9')
+			num = (num * 10) + (s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (s++);
+
+	return (num * sign);
 }
