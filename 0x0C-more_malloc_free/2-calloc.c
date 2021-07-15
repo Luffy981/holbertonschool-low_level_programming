feead6582a65f8137ec1028ce66fbf00bb5fc3f5
luffy981 <jhonsmith981@hotmail.com>
2021-07-15 23:49:05 +0000

    Function to reserve memory and set with 0

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _calloc - Function to reserve memory and set with 0
+ * @nmemb: Parameter
+ * @size: Parameter
+ * Return: Return value
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int i;
+
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(size) * nmemb);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < nmemb ; i++)
+	{
+		*(p + i) = 0;
+	}
+	return (p);
+}
