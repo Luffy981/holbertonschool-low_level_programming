f43cd7779d6c717a62abbbcda89202f1ae94cfbb
luffy981 <jhonsmith981@hotmail.com>
2021-07-20 21:37:42 +0000

    Define a new type dog_t as a new name for the type struct dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - Function to create a new dog
+ * @name: Parameter
+ * @owner: Parameter
+ * @age: Parameter
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	dog = malloc((sizeof(struct my_dog)));
+	if(dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
