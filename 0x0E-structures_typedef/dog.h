5e9281135d91099cdbb3ab0f7a2b16e3dc4c09b1
luffy981 <jhonsmith981@hotmail.com>
2021-07-20 05:40:35 +0000

    Struc Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef STRUC_DOG
+#define STRUC_DOG
+/**
+ * struct dog - Description about dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: Information about the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
