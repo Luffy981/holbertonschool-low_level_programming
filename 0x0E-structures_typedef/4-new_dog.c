141ab27a3beb26e01c52207e5e5ae1346b931bb4
luffy981 <jhonsmith981@hotmail.com>
2021-07-21 00:35:05 +0000

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,13 +6,18 @@
  * @name: Parameter
  * @owner: Parameter
  * @age: Parameter
+ * Return: Return value dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+
 	dog = malloc((sizeof(dog_t)));
-	if(dog == NULL)
+	if (dog == NULL)
+	{
+		free(dog);
 		return (NULL);
+	}
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
