06f1cd9d9f6462b749fd78e0c3d7e99ab54d7b7f
luffy981 <jhonsmith981@hotmail.com>
2021-07-22 02:53:14 +0000

    File headers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 int op_add(int a, int b);
 int op_sub(int a, int b);
