16abbddc3d099d62dd46fcc9c968924c64830ace
luffy981 <jhonsmith981@hotmail.com>
2021-07-22 02:21:28 +0000

    Print the namme from pointer to function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - Function the name
+ * @name: Parameter
+ * @f: Pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
