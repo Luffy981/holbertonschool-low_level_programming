4bf226e3529e9200757952925f216c334d7790a9
luffy981 <jhonsmith981@hotmail.com>
2021-07-23 05:43:59 +0000

    function that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - function that prints numbers
+ * @n: Array to ecaluate
+ * @separator: Array to separate numbers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list numbers;
+	unsigned int i;
+
+	va_start(numbers, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", va_arg(numbers, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(numbers);
+}
