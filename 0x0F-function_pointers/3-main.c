81da026dd65d8d09845869e3451d3485f02294c8
luffy981 <jhonsmith981@hotmail.com>
2021-07-23 18:03:17 +0000

    Function main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,17 +10,18 @@
 int main(int argc, char *argv[])
 {
 	int b, c;
-	char *a = argv[2];
+	char *a;
 
-	b = atoi(argv[1]);
-	c = atoi(argv[3]);
-	if (argv[2][1] != '\0' || get_op_func(a) == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
 	if (argc == 4)
 	{
+		a = argv[2];
+		b = atoi(argv[1]);
+		c = atoi(argv[3]);
+		if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+		{
+			printf("Error\n");
+			exit(99);
+		}
 		return (printf("%d\n", get_op_func(a)(b, c)));
 	}
 	printf("Error\n");
