f0f663c4645b8553ddfaf60b1604b7b34c562597
luffy981 <jhonsmith981@hotmail.com>
2021-08-03 05:25:34 +0000

    Function to print before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_str - function to print before the main function
+ */
+void printf_str(void) __attribute__((constructor));
+
+void printf_str(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
