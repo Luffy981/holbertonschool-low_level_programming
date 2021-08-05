74c4c979b1299f06a765927ed793a6de99299c9b
luffy981 <jhonsmith981@hotmail.com>
2021-08-05 01:11:29 +0000

    Free linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * free_listint - function to free memory
+ * @head: head
+ *
+ * Return: Return count
+ */
+void free_listint(listint_t *head)
+{
+	if (!head)
+	{
+		return;
+	}
+	if (head->next)
+	{
+		free_listint(head->next);
+	}
+	free(head);
+}
+/**
+ * free_listint2 - function to free memory
+ * @head: head
+ *
+ * Return: Return count
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p = *head;
+
+	if (!(p))
+	{
+		return;
+	}
+	free_listint(p);
+}
