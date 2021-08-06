f2cfa0f79fa35f72e285f95a3fdc7cab5ef189c4
luffy981 <jhonsmith981@hotmail.com>
2021-08-06 05:43:56 +0000

    Binary string to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	const char *a = b;
 	int c = 0;
 	int m;
+	int i = 0;
 	unsigned int f = 1;
 	unsigned int n = 0;
 
 	if (b == NULL)
 		return (0);
-	while (*a)
+	while (b[c] != '\0')
 	{
-		if (*a != '0' && *a != '1')
+		if (b[c] != '0' && b[c] != '1')
 		{
 			return (0);
 		}
-		a++;
 		c++;
 	}
 	m = c;
@@ -32,10 +31,10 @@ unsigned int binary_to_uint(const char *b)
 	}
 	while (m > 0)
 	{
-		n += ((*b - '0') * f);
+		n += ((b[i] - '0') * f);
 		f /= 2;
 		m--;
-		b++;
+		i++;
 	}
 	return (n);
 }
