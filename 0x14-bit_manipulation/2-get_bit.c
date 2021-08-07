bfa2868b43552e0ae74b9eec3995fa2bd2c2873c
luffy981 <jhonsmith981@hotmail.com>
2021-08-07 01:27:36 +0000

    function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 		return ((n & 1));
 	}
 	if (n < 2 && index > 0)
-		return (-1);
+		return (0);
 	if (n > 1 || index == 0)
 	{
 		count = get_bit(n >> 1, (index - 1));
