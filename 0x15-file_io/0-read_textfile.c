1f0562ca7d42775cbbb52c0b3e22dbff55033531
luffy981 <jhonsmith981@hotmail.com>
2021-08-09 21:36:23 +0000

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,15 +11,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buff;
 
 	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
 	POSIX = open(filename, O_RDONLY);
-	if (POSIX == -1)
+	if (POSIX == -1 | filename == NULL)
 		return (0);
-	if (filename == NULL)
-	{
-		return (0);
-	}
 	r = read(POSIX, buff, letters);
+	if (r == -1)
+		return (0);
 	close(POSIX);
 	write(STDOUT_FILENO, buff, letters);
+	if (write == -1)
+		return (0);
 	return (r);
 }
