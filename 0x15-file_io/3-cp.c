ee8de60a059ccd6ffb2a2b4d4f147f3e2b975d79
luffy981 <jhonsmith981@hotmail.com>
2021-08-10 04:51:01 +0000

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,18 +18,15 @@ void copy_file(const char *file_from, const char *file_to)
 		exit(98);
 	}
 	r = read(fd, buff, 1024);
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
 	df = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (df == -1)
+	while (r > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
+		if (write(df, buff, r) != r | df == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			exit(99);
+		}
 	}
-	write(df, buff, r);
 	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
