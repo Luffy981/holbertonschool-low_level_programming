375afae804b8f7ce8f2a686dc53814b924654de3
luffy981 <jhonsmith981@hotmail.com>
2021-08-10 00:24:08 +0000

    Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,12 +10,13 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int count = 0;
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1 || filename == NULL)
 		return (-1);
 	while (text_content[count] != '\0')
 		count++;
-	write(fd, text_content, count);
+	if (text_content != NULL)
+		write(fd, text_content, count);
 	close(fd);
 	return (1);
 }
