1e4b9e962df7da61bfddc9560a5708c713c05a63
luffy981 <jhonsmith981@hotmail.com>
2021-08-10 04:28:25 +0000

    program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ * copy_file - program that copies the content of a file to another file
+ * @file_from:  file from copy
+ * @file_to: file to copy
+ */
+void copy_file(const char *file_from, const char *file_to)
+{
+	int fd;
+	int df;
+	char buff[1024];
+	int r;
+
+	fd = open(file_from, O_RDONLY);
+	if (!file_from || fd == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	r = read(fd, buff, 1024);
+	if (r == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	df = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (df == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	write(df, buff, r);
+	if (close(fd) == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	} else
+		close(fd);
+	if (close(df) == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", df);
+		exit(100);
+	} else
+		close(df);
+}
+/**
+ * main - function main
+ * @argc:  Argument
+ * @argv: Argument
+ * Return: Return count
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	copy_file(argv[1], argv[2]);
+	exit(0);
+}
