81b193af20489c66c95c916f1f5ea175db28b4e3
luffy981 <jhonsmith981@hotmail.com>
2021-09-16 15:55:08 -0500

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/*
+ * print_dlistint - function to print all the elements of a list_t list
+ * @h: Pointer of type list_t
+ *
+ * Return: Return count
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
     int count = 0;
