fb543cb80939f255e502017322cad5cebcb86eb1
luffy981 <jhonsmith981@hotmail.com>
2021-09-16 20:06:00 -0500

    Insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,52 +1,39 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - function insert node at index
+ * insert_dnodeint_at_index - function insert node at index
  * @idx : index of the node, starting at 0
  * @head: Pointer to  head list
  * @n: number
  *
  * Return: Return count
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
-{
-	dlistint_t *newnode;
-	dlistint_t *node = *head;
-	dlistint_t *ar;
-	dlistint_t *h = *head;
-	unsigned int i = 0;
-	unsigned int count = 0;
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n) {
+	unsigned int i;
+	dlistint_t *current, *new;
 
-	if (*head == NULL && idx != 0)
+	if (h == NULL)
 		return (NULL);
-	while (h)
+	if (idx != 0)
 	{
-		h = h->next;
-		count++;
+		current = *h;
+		for (i = 0; i < idx - 1 && current != NULL; i++)
+		{
+			current = current->next;
+		}
+		if (current == NULL)
+			return (NULL);
 	}
-	if (count < idx)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
+	new->n = n;
 	if (idx == 0)
 	{
-		newnode = malloc(sizeof(dlistint_t));
-		if (newnode == NULL)
-			return (NULL);
-		newnode->n = n;
-		newnode->next = *head;
-		*head = newnode;
-	} else
-	{
-		while (i < idx - 1)
-		{
-			node = node->next;
-			i++;
-		}
-		ar = node->next;
-		newnode = malloc(sizeof(dlistint_t));
-		if (newnode == NULL)
-			return (NULL);
-		newnode->n = n;
-		newnode->next = ar;
-		node->next = newnode;
+		new->next = *h;
+		*h = new;
+		return (new);
 	}
-	return (newnode);
+	new->next = current->next;
+	current->next = new;
+	return (new);
 }
