6c7be97693ad7f5848611169cd2c9eefe1b33f69
luffy981 <jhonsmith981@hotmail.com>
2021-10-08 18:59:29 -0500

    hash table created

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,10 +6,24 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	unsigned long int i = 0;
+	hash_table_t *hash_table = NULL;
 
-	table = malloc(sizeof(hash_table_t) * size);
-	if (table == NULL)
+	if (size <= 0)
 		return (NULL);
-	return (table);
+	hash_table = malloc(sizeof(hash_table_t) * 1);
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array =  malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	for (; i < size ; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+	return (hash_table);
 }
