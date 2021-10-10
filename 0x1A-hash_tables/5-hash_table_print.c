b0643673eeac046cade37d11f1478f1838c02172
luffy981 <jhonsmith981@hotmail.com>
2021-10-10 11:21:48 -0500

    print  hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,17 +8,22 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *hash_node = NULL;
 	unsigned long int i = 0;
+	int flag = 1;
 
 	if (!ht)
 		return;
 	putchar('{');
 	while (i < ht->size)
 	{
+		
 		hash_node = ht->array[i];
 		while (hash_node)
 		{
+			if (flag == 0)
+			printf(", ");
 			printf("'%s': '%s'", hash_node->key, hash_node->value);
 			hash_node = hash_node->next;
+			flag = 0;
 		}
 		i++;
 	}
