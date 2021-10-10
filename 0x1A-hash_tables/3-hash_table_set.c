3ca320c50f85f3bc13ed8ddafeade63d7dc7a82c
luffy981 <jhonsmith981@hotmail.com>
2021-10-09 21:12:26 -0500

    hash table set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *hash_node = NULL;
+	hash_node_t *new_node = NULL;
+	char *new_value = NULL;
+	char *new_key = NULL;
+
+	unsigned long int key_idx;
+	if (!ht || !key || !value)
+		return (0);
+	key_idx = key_index((const unsigned char *)key, ht->size); 
+	new_value = strdup(value);
+	hash_node = ht->array[key_idx];
+	while (hash_node)
+	{
+		if (strcmp(hash_node->key, key) == 0)
+		{
+			hash_node->value = new_value;
+			return (1);
+		}
+		hash_node = hash_node->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+	new_key = strdup(key);
+	new_node->key = new_key;
+	new_node->value = new_value;
+	new_node->next = ht->array[key_idx];
+	ht->array[key_idx] = new_node;
+	return (1);
+
+}
