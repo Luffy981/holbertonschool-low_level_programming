f075c69f7cb7996fde72015a0963a35122f49df1
luffy981 <jhonsmith981@hotmail.com>
2021-10-17 13:47:40 -0500

    rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * rot13 - Function to rot
+ * @string: Parameter
+ * Return: return value
+ */
+
+char *rot13(char *string)
+{
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *ptr1 = NULL;
+	char *ptr2 = NULL;
+	char *string1 = string;
+
+	while (*string1 != '\0')
+	{
+		for (ptr1 = input, ptr2 = output; *ptr1 != '\0'; ptr1++, ptr2++)
+		{
+			if (*string1 == *ptr1)
+			{
+				*string1 = *ptr2;
+				break;
+			}
+		}
+		string1++;
+	}
+	return (string);
+
+}
