0057a5cc6fd910ccc743cbd6f6dbf112fe520899
luffy981 <jhonsmith981@hotmail.com>
2022-01-17 16:39:29 -0500

    linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+/**
+ * linear_search - linear_search
+ * @array: array
+ * @size: size of array
+ * @value: value
+ *
+ * Return: index of value
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+
+	if (array == NULL)
+		return (-1);
+	while (i < (int)size)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		i++;
+	}
+	return (-1);
+}
