d4981e03f94f44b28438a61e79eb42d6f4929c45
luffy981 <jhonsmith981@hotmail.com>
2022-01-19 22:15:20 -0500

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,45 +1,51 @@
 #include "search_algos.h"
+
 /**
- * linear_search - linear_search
- * @array: array
- * @size: size of array
- * @value: value
+ * print_subarr - Prints a subarray between two indexes
+ * @arr: Array of integers
+ * @l: Left index
+ * @r: Right index
+ */
+void print_subarr(int *arr, int l, int r)
+{
+	int i;
+	char *sep = "";
+
+	printf("Searching in array: ");
+	for (i = l; i <= r; i++)
+	{
+		printf("%s%d", sep, arr[i]);
+		sep = ", ";
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search - Searches a value in an array of integers
+ * @array: Array of integers
+ * @size: Array's size
+ * @value: Value to search
  *
- * Return: index of value
+ * Return: Index at which the value was found, -1 otherwise
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int left = 0;
-	int right = size - 1;
-	int mid;
-	int i = 0;
-	int flag = 0;
+	int mid, l = 0, r = (int)size - 1;
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
-	while (left < right)
+
+	while (l <= r)
 	{
-		i = left;
-		flag = 0;
-		printf("Searching in array: ");
-		while (i <= right)
-		{
-			if (flag == 1)
-				printf(", ");
-			printf("%d", array[i]);
-			flag = 1;
-			i++;
-		}
-		printf("\n");
-		mid = (right + left) / 2;
+		print_subarr(array, l, r);
+		mid = (l + r) / 2;
 		if (value == array[mid])
 			return (mid);
 		else if (value < array[mid])
-			right = mid - 1;
-		else
-			left = mid + 1;
+			r = mid - 1;
+		else if (value > array[mid])
+			l = mid + 1;
 	}
 
-	printf("Searching in array: %d\n", array[mid + 1]);
 	return (-1);
 }
